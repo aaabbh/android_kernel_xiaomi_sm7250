255ce03eaf31c6ff9e8d6b04ac18c5e288851a91
hoang1007 <hoangvuhuy1007@gmail.com>
2025-05-23 16:01:32 +0700

    kernel/sysctl: Fix unused var warnings
    ../kernel/sysctl.c:385:12: warning: unused variable 'min_sched_granularity_ns' [-Wunused-variable]
      385 | static int min_sched_granularity_ns = 100000;           /* 100 usecs */
          |            ^~~~~~~~~~~~~~~~~~~~~~~~
    ../kernel/sysctl.c:386:12: warning: unused variable 'max_sched_granularity_ns' [-Wunused-variable]
      386 | static int max_sched_granularity_ns = NSEC_PER_SEC;     /* 1 second */
          |            ^~~~~~~~~~~~~~~~~~~~~~~~
    ../kernel/sysctl.c:387:12: warning: unused variable 'min_wakeup_granularity_ns' [-Wunused-variable]
      387 | static int min_wakeup_granularity_ns;                   /* 0 usecs */
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~
    ../kernel/sysctl.c:388:12: warning: unused variable 'max_wakeup_granularity_ns' [-Wunused-variable]
      388 | static int max_wakeup_granularity_ns = NSEC_PER_SEC;    /* 1 second */
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~
    ../kernel/sysctl.c:390:12: warning: unused variable 'min_sched_tunable_scaling' [-Wunused-variable]
      390 | static int min_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~
    ../kernel/sysctl.c:391:12: warning: unused variable 'max_sched_tunable_scaling' [-Wunused-variable]
      391 | static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -381,14 +381,16 @@ static struct ctl_table sysctl_base_table[] = {
 	{ }
 };
 
+#ifdef CONFIG_SCHED_DEBUG
+#if !defined(CONFIG_SCHED_BORE)
 static int min_sched_granularity_ns = 100000;		/* 100 usecs */
 static int max_sched_granularity_ns = NSEC_PER_SEC;	/* 1 second */
-static int min_wakeup_granularity_ns;			/* 0 usecs */
-static int max_wakeup_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 #ifdef CONFIG_SMP
 static int min_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
 #endif /* CONFIG_SMP */
+#endif /* CONFIG_SCHED_BORE */
+#endif /* CONFIG_SCHED_DEBUG */
 
 #ifdef CONFIG_COMPACTION
 static int min_extfrag_threshold;
