872301fd454c1831387152f684a554c4997e05e4
hoang1007 <hoangvuhuy1007@gmail.com>
2025-06-07 21:55:11 +0700

    zram: Allow to change comp algorithm from userspace
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1111,7 +1111,6 @@ static ssize_t __comp_algorithm_show(struct zram *zram, u32 prio, char *buf)
 
 static int __comp_algorithm_store(struct zram *zram, u32 prio, const char *buf)
 {
-#if 0
 	char *compressor;
 	size_t sz;
 
@@ -1132,7 +1131,6 @@ static int __comp_algorithm_store(struct zram *zram, u32 prio, const char *buf)
 		return -EINVAL;
 	}
 
-	return len; //zram: disallow userspace to change zram compression algorithm
 	down_write(&zram->init_lock);
 	if (init_done(zram)) {
 		up_write(&zram->init_lock);
@@ -1143,7 +1141,6 @@ static int __comp_algorithm_store(struct zram *zram, u32 prio, const char *buf)
 
 	comp_algorithm_set(zram, prio, compressor);
 	up_write(&zram->init_lock);
-#endif
 	return 0;
 }
 
